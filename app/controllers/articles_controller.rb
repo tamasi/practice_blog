4f4723ae0056083a292a917c338f6c14566cdf38
hernan <hernan.bordon@gmail.com>
2017-02-21 17:18:17 -0300

    First commit - Blog project to practice

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+	before_filter :find_model
+	#GET /articles
+	def index 
+		@articles = Article.all
+	end
+	
+	#GET /articles/:id
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	#GET /articles/new
+	def new
+		@article = Article.new
+	end
+
+	#POST /articles
+	def create
+		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+
+	#PUT /articles/update
+	def update
+		
+	end
+
+	private
+	def find_model
+		@model = Article.find(params[:id]) if params[:id]
+	end
+end
