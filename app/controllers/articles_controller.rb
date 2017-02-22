2961af1ab9e321b0d19ab998dec36debfa2b703e
hernan <hernan.bordon@gmail.com>
2017-02-22 12:00:02 -0300

    - Add partial for form in Articles views - Add edit view - change new article view - fix routing errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,15 @@ class ArticlesController < ApplicationController
 
 	#PUT /articles/update
 	def update
-		
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+	end
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def destroy
