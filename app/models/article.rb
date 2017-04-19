77d770beb7d60ea0ac64ace58868f967e3069da3
tamasi <hernan.bordon@gmail.com>
2017-04-19 12:18:35 -0300

    - fix issue with articles destroy. I add dependent destroy to comments relation in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 	include AASM
 
 	belongs_to :user
-	has_many :comments
+	has_many :comments, :dependent => :destroy
 
 	has_many :has_categories, :dependent => :destroy
 	has_many :categories, through: :has_categories
