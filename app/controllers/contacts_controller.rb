0aa42901027752ac935d2b48c1fe09ce6ba8d68b
tamasi <hernan.bordon@gmail.com>
2017-04-21 18:07:05 -0300

    - Add Contact model - Add contacts_controller - Add contacts views for contact#new - Add form for contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+	before_action :set_joinus, only:[:new, :create]
+	def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(secure_params)
+		if @contact.valid?
+			# ToDo send messages
+			flash[:notice] = "Message sent from #{@contact.name}."
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def secure_params
+		params.require(:contact).permit(:name, :email, :content)
+	end
+end
